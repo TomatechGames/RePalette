411bc7fe5e073019f8cede396c318a982aa4a6cc
TomatechGames <theludwiggamer@gmail.com>
2023-05-01 00:24:04 +0100

    Added Namespace to BasicThemeAssetLoader

--- Runtime/BasicThemeAssetLoader.cs
+++ Runtime/BasicThemeAssetLoader.cs
@@ -5,38 +5,40 @@ using UnityEngine;
 using UnityEngine.Events;
 using Tomatech.RePalette;
 
-
-public class ThemeAssetLoader : MonoBehaviour, IThemeable
+namespace Tomatech.RePalette
 {
-    [SerializeField]
-    ThemeAssetReference<Sprite> assetRef;
-    [SerializeField]
-    UnityEvent<Sprite> onThemeSpriteRecieved;
-
-    public async Task UpdateThemeContent()
+    public class ThemeAssetLoader : MonoBehaviour, IThemeable
     {
-        var assetTask = assetRef?.GetAsset();
-        if (assetTask != null)
+        [SerializeField]
+        ThemeAssetReference<Sprite> assetRef;
+        [SerializeField]
+        UnityEvent<Sprite> onThemeSpriteRecieved;
+
+        public async Task UpdateThemeContent()
         {
-            await assetTask;
-            onThemeSpriteRecieved.Invoke(assetTask.Result);
+            var assetTask = assetRef?.GetAsset();
+            if (assetTask != null)
+            {
+                await assetTask;
+                onThemeSpriteRecieved.Invoke(assetTask.Result);
+            }
         }
-    }
 
-    private void OnEnable()
-    {
-        RepaletteResourceManager.RegisterThemeable(this);
-        var _ = UpdateThemeContent();
-    }
+        private void OnEnable()
+        {
+            RepaletteResourceManager.RegisterThemeable(this);
+            var _ = UpdateThemeContent();
+        }
 
-    private void OnDisable()
-    {
-        RepaletteResourceManager.UnregisterThemeable(this);
-    }
+        private void OnDisable()
+        {
+            RepaletteResourceManager.UnregisterThemeable(this);
+        }
 
-    private void OnDestroy()
-    {
-        assetRef.ReleaseAsset();
-    }
+        private void OnDestroy()
+        {
+            assetRef.ReleaseAsset();
+        }
 
+    }
 }
